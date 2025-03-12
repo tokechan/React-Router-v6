7e48a116990578699f64f5f04e939a4bdf02c29e
tokechan <fleatoke@gmail.com>
2025-03-12 23:43:47 +0900

    Add:type setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Route, Router, Routes } from "react-router-dom";
+
 
 function App() {
-  
   return (
-    <>
-     <h1>Todo App</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todo/new" element={<TodoCreate />} />  
+        <Route path="/todo/:id" element={<TodoDetail  />} />  
+        <Route path="/todo/:id/edit" element={<TodoEdit  />} />  
+        <Route path="*" element={<NotFound />} />  
+      </Routes>
+    </Router>
+    
   )
 }
-
-export default App
