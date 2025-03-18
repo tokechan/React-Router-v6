96e348ca7734a6def0d9a7b2c99501d261161e1e
tokechan <fleatoke@gmail.com>
2025-03-18 21:58:17 +0900

    Add: Laravel from docker

---
+++ backend/database/migrations/2025_03_18_115700_create_memos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->id();
+            $table->text('content')->comment('メモの内容');
+            $table->string('status')->default('メモっとくね');
+            $table->string('creator')->comment('作成者（夫/妻）');
+            $table->boolean('completed')->default(false)->comment('完了フラグ');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memos');
+    }
+};
